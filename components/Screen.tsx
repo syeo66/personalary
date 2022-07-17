a950f2e48d7fee3f46497887c7fdc8a6f14356d0
Red Ochsenbein <red@redochsenbein.ch>
2022-07-17 16:16:05 +0200

    Setup styled components

---
+++ components/Screen.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Screen = styled.div`
+  background-color: #111;
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+
+export default Screen
