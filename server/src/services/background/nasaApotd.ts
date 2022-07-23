f4dd3f098c196fe2fa29136bd50136c7ef92ff80
Red Ochsenbein <red@redochsenbein.ch>
2022-07-23 18:59:44 +0200

    Streamline rxjs pipe and prevent crash on failed requests

--- server/src/services/background/nasaApotd.ts
+++ server/src/services/background/nasaApotd.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { concatMap, distinctUntilChanged, filter, from, map, timer } from 'rxjs'
+import { catchError, concatMap, distinctUntilChanged, filter, from, map, timer } from 'rxjs'
 
 import config from '../../config'
 
@@ -8,22 +8,20 @@ const { apiKey, refetchInterval, rotationInterval } = config.background
 const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=20`
 
 const nasaApotd = () => {
-  return timer(0, refetchInterval * 1000)
-    .pipe(concatMap(() => from(axios.get(url))))
-    .pipe(
-      map((res) => {
-        return res.data?.map(({ hdurl }: { hdurl: string }) => hdurl).filter(Boolean)
-      })
-    )
-    .pipe(
-      concatMap((ev) =>
-        timer(0, rotationInterval * 1000).pipe(
-          map((i) => (ev[i % ev.length] ? `SetBackground ${ev[i % ev.length]}` : null))
-        )
+  return timer(0, refetchInterval * 1000).pipe(
+    concatMap(() => from(axios.get(url))),
+    catchError((err, caught) => caught),
+    map((res) => {
+      return res.data?.map(({ hdurl }: { hdurl: string }) => hdurl).filter(Boolean)
+    }),
+    concatMap((ev) =>
+      timer(0, rotationInterval * 1000).pipe(
+        map((i) => (ev[i % ev.length] ? `SetBackground ${ev[i % ev.length]}` : null))
       )
-    )
-    .pipe(filter(Boolean))
-    .pipe(distinctUntilChanged())
+    ),
+    filter(Boolean),
+    distinctUntilChanged()
+  )
 }
 
 export default nasaApotd
--- server/src/services/messages/csvDownload.ts
+++ server/src/services/messages/csvDownload.ts
@@ -1,69 +1,67 @@
 import axios from 'axios'
 import { parse } from 'csv-parse/sync'
 import { compareDesc, parse as parseDate } from 'date-fns'
-import { concatMap, distinctUntilChanged, filter, from, map, timer } from 'rxjs'
+import { catchError, concatMap, distinctUntilChanged, EMPTY, filter, from, map, timer } from 'rxjs'
 
 import config from '../../config'
 
 const { url, refetchInterval, rotationInterval, dateFormat } = config.messages
 
 const csvDownload = () => {
-  return timer(0, refetchInterval * 1000)
-    .pipe(concatMap(() => from(axios.get(url))))
-    .pipe(
-      map((res) => {
-        if (!res.data) {
-          return []
-        }
-        return parse(res.data)
-      })
-    )
-    .pipe(
-      concatMap((ev) =>
-        timer(0, rotationInterval * 1000).pipe(
-          map(() => {
-            const now = new Date()
+  if (!url) {
+    return EMPTY
+  }
 
-            const mapped = (ev as string[][]).map(
-              ([fromDate, toDate, message]: string[]): [Date, Date | null, string] => {
-                return [
-                  parseDate(fromDate, dateFormat, now),
-                  toDate ? parseDate(toDate, dateFormat, now) : null,
-                  message,
-                ]
-              }
-            )
-            mapped.sort((a, b) => {
-              const diff = compareDesc(a[0], b[0])
+  return timer(0, refetchInterval * 1000).pipe(
+    concatMap(() => from(axios.get(url))),
+    catchError((err, caught) => caught),
+    map((res) => {
+      if (!res.data) {
+        return []
+      }
+      return parse(res.data)
+    }),
+    concatMap((ev) =>
+      timer(0, rotationInterval * 1000).pipe(
+        map(() => {
+          const now = new Date()
 
-              if (diff === 0) {
-                return compareDesc(a[1] || now, b[1] || now)
-              }
+          const mapped = (ev as string[][]).map(
+            ([fromDate, toDate, message]: string[]): [Date, Date | null, string] => {
+              return [parseDate(fromDate, dateFormat, now), toDate ? parseDate(toDate, dateFormat, now) : null, message]
+            }
+          )
+          mapped.sort((a, b) => {
+            const diff = compareDesc(a[0], b[0])
 
-              return diff
-            })
-
-            const resp = mapped.filter(([fromDate, toDate, message]) => {
-              if (!message) {
-                return false
-              }
-
-              return fromDate < new Date() && (toDate === null || toDate > new Date())
-            })
-
-            if (!resp?.[0]?.[2]) {
-              return null
+            if (diff === 0) {
+              return compareDesc(a[1] || now, b[1] || now)
             }
 
-            const [[fromDate, toDate, message]] = resp
-
-            return `ShowMessage ${JSON.stringify({ fromDate, toDate, message })}`
+            return diff
           })
-        )
+
+          const resp = mapped.filter(([fromDate, toDate, message]) => {
+            if (!message) {
+              return false
+            }
+
+            return fromDate < new Date() && (toDate === null || toDate > new Date())
+          })
+
+          if (!resp?.[0]?.[2]) {
+            return null
+          }
+
+          const [[fromDate, toDate, message]] = resp
+
+          return `ShowMessage ${JSON.stringify({ fromDate, toDate, message })}`
+        })
       )
-    )
-    .pipe(filter((message): message is string => typeof message === 'string'))
-    .pipe(distinctUntilChanged())
+    ),
+    filter((message): message is string => typeof message === 'string'),
+    distinctUntilChanged()
+  )
 }
 
 export default csvDownload
