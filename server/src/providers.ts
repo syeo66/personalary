371e5a389c1f4adf1e2ec212fb8bb791a8d6816a
Red Ochsenbein <red@redochsenbein.ch>
2022-08-08 20:59:56 +0200

    Refactor providers loading

--- server/src/providers.ts
+++ server/src/providers.ts
@@ -1,9 +1,18 @@
+import { pipe } from 'ramda'
 import { Observable } from 'rxjs'
 
 import nasaApotd from './services/background/nasaApotd'
 import clockConfig from './services/clock/config'
 import csvDownload from './services/messages/csvDownload'
 
-const providers: Observable<string>[] = [nasaApotd(), csvDownload(), clockConfig()]
+type Service = Observable<string>
+
+const getMessagesService = (prev: Service[]) => [...prev, csvDownload()]
+const getBackgroundService = (prev: Service[]) => [...prev, nasaApotd()]
+const getClockService = (prev: Service[]) => [...prev, clockConfig()]
+
+const getServices = pipe(getClockService, getBackgroundService, getMessagesService)
+
+const providers = getServices([])
 
 export default providers
