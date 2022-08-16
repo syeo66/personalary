833f00bb9a2dd8e48152a2f86bb210d0738e930b
Red Ochsenbein <red@redochsenbein.ch>
2022-08-16 14:28:26 +0200

    Refresh token from spotify

--- server/src/services/musicPlayer/spotifyRemote.ts
+++ server/src/services/musicPlayer/spotifyRemote.ts
@@ -1,13 +1,28 @@
+import axios from 'axios'
+import fs from 'fs'
 import { distinctUntilChanged, map, timer } from 'rxjs'
 
+import spotifyRemoteConfig, { configFilePath, configPath } from '../../configs/spotifyRemoteConfig'
 import loadConfig from '../../loadConfig'
 import MusicPlayerData, { MusicPlayerDataType } from './MusicPlayerData'
 
 const { musicPlayer: config } = loadConfig()
 
-const MockPlayer = () => {
+const SpotifyRemote = () => {
   return timer(500, 1000).pipe(
     map((v) => {
+      const spotifyConfig = spotifyRemoteConfig()
+
+      if (!spotifyConfig?.access_token) {
+        return { enabled: false }
+      }
+
+      const { timestamp, expires_in, refresh_token } = spotifyConfig
+      const now = Math.floor(Date.now() / 1000)
+      if (now > timestamp + expires_in - 600) {
+        refresh(refresh_token)
+      }
+
       const data: MusicPlayerDataType = {
         artist: {
           name: 'Red Ochsenbein',
@@ -34,4 +49,36 @@ const MockPlayer = () => {
   )
 }
 
-export default MockPlayer
+const clientSecret = process.env.SPOTIFY_API_SECRET_KEY
+
+const refresh = async (refresh_token: string) => {
+  const { clientId } = loadConfig().musicPlayer
+
+  const authOptions = {
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    withCredentials: true,
+  }
+
+  try {
+    const resp = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      new URLSearchParams({ refresh_token, grant_type: 'refresh_token' }),
+      authOptions
+    )
+
+    if (!fs.existsSync(configPath)) {
+      fs.mkdirSync(configPath)
+    }
+    fs.writeFileSync(
+      configFilePath,
+      JSON.stringify({ ...resp.data, timestamp: Math.floor(Date.now() / 1000) }, null, '  ')
+    )
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export default SpotifyRemote
