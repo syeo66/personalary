2dd523a0391a0c4627e1e1c03e2ec4b4d6feeadf
Red Ochsenbein <red@redochsenbein.ch>
2022-11-01 14:29:59 +0100

    Improve text alignment in digital clock

--- front/src/components/widgets/Clock/DigitalClock.tsx
+++ front/src/components/widgets/Clock/DigitalClock.tsx
@@ -19,23 +19,31 @@ const DigitalClock: React.FC<DigitalClockProps> = ({ config }) => {
     return () => clearInterval(interval)
   }, [])
 
+  const align = config.position.includes('right') ? 'right' : 'left'
+
   return (
     <>
-      <Time>{format(time, config.timeFormat)}</Time>
-      <DateView>{format(time, config.dateFormat)}</DateView>
+      <Time align={align}>{format(time, config.timeFormat)}</Time>
+      <DateView align={align}>{format(time, config.dateFormat)}</DateView>
     </>
   )
 }
 
-const Time = styled.div`
-  font-size: 4rem;
+interface Props {
+  align?: 'left' | 'right'
+}
+
+const Time = styled.div<Props>`
   display: flex;
-  justify-content: flex-start;
+  font-size: 4rem;
+  justify-content: ${({ align }) => (align === 'right' ? 'flex-end' : 'flex-start')};
   text-shadow: 0 0 5px rgba(0, 0, 0, 0.5), 0 0 10px rgba(0, 0, 0, 0.4), 0 0 15px rgba(0, 0, 0, 0.3);
 `
 
-const DateView = styled.div`
+const DateView = styled.div<Props>`
+  display: flex;
   font-size: 1.5rem;
+  justify-content: ${({ align }) => (align === 'right' ? 'flex-end' : 'flex-start')};
   text-shadow: 0 0 5px rgba(0, 0, 0, 0.5), 0 0 10px rgba(0, 0, 0, 0.4), 0 0 15px rgba(0, 0, 0, 0.3);
 `
 
