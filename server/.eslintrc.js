f8568fda9c292ede312add3df622eb21cf2f90ed
Red Ochsenbein <red@redochsenbein.ch>
2022-12-20 20:32:18 +0100

    Fix eslintrc

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,1 +1,33 @@
-node_modules
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { modules: true },
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier', 'simple-import-sort'],
+  root: true,
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
+    complexity: ['warn', 10],
+    eqeqeq: 'error',
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 'off',
+    'no-else-return': 'error',
+    'no-empty': 'error',
+    'no-shadow': 'error',
+    'prefer-destructuring': 'error',
+    'prefer-template': 'error',
+    'prettier/prettier': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-vars': 'error',
+  },
+}
