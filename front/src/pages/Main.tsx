d603e3aa913b8f2b85bc674c60a558b15b0ae77c
Red Ochsenbein <red@redochsenbein.ch>
2023-01-11 19:46:13 +0100

    Add reload after a random time after between one and two days

--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import Background from '../components/Background'
 import Screen from '../components/Screen'
@@ -8,17 +8,29 @@ import MusicPlayer from '../components/widgets/MusicPlayer'
 import Weather from '../components/widgets/Weather'
 import WebsocketProvider from '../providers/WebsocketProvider'
 
-const Main: React.FC = () => (
-  <WebsocketProvider>
-    <Screen>
-      <Background>
-        <Clock />
-        <Message />
-        <MusicPlayer />
-        <Weather />
-      </Background>
-    </Screen>
-  </WebsocketProvider>
-)
+const DAY = 60 * 60 * 24 * 1000
+
+const Main: React.FC = () => {
+  useEffect(() => {
+    // reload page after a random time between 1 and 2 days
+    // this should prevent memory issues because of long running times
+    const timeout = setTimeout(() => document.location.reload(), DAY + Math.trunc(Math.random() * DAY))
+
+    return () => clearTimeout(timeout)
+  }, [])
+
+  return (
+    <WebsocketProvider>
+      <Screen>
+        <Background>
+          <Clock />
+          <Message />
+          <MusicPlayer />
+          <Weather />
+        </Background>
+      </Screen>
+    </WebsocketProvider>
+  )
+}
 
 export default Main
